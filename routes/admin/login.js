39db83ef6c2c29646bd9be9ab25e0c9f22a94fa1
Bibiana <lucerina23@gmail.com>
2022-07-27 15:38:28 -0300

    login y logout

--- routes/admin/login.js
+++ routes/admin/login.js
@@ -26,9 +26,10 @@ router.post('/', async (req, res, next) => {
         var data = await usuariosModel.getUserAndPassword(usuario, password);
 
         if (data != undefined) {
-            res.redirect('/admin/novedades');
+            
             req.session.id_usuario = data.id;
             req.session.nombre = data.usuario;
+            res.redirect('/admin/novedades');
             
         } else {
             res.render('admin/login', {
