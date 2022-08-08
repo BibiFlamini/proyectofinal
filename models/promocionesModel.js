4e83789c48d1987f1f53b60c9315af16e10e564c
Bibiana <lucerina23@gmail.com>
2022-08-08 08:33:37 -0300

    agregar y modificar

--- models/promocionesModel.js
+++ models/promocionesModel.js
@@ -13,4 +13,33 @@ async function deletePromocionesById(id) {
         return rows;
 }
 
-module.exports = { getPromociones, deletePromocionesById }
+async function insertPromociones(obj) {
+        try {
+                var query = "insert into promociones set ?";
+                var rows = await pool.query(query, [obj])
+                return rows;    
+        } catch (error) {
+                console.log(error);
+                throw error;
+        }    
+}
+
+async function getPromocionesById(id) {
+        var query = "select * from promociones where id = ? ";
+        var rows = await pool.query(query, [id]);
+        return rows[0];
+}
+
+async function modificarPromocionesById(obj, id) {
+        try {
+                var query = "update promociones set ? where id=?";
+                var rows = await pool.query(query, [obj, id]);
+                return rows;
+        } catch (error) {
+                throw error;
+        }
+}
+
+
+
+module.exports = { getPromociones, deletePromocionesById, insertPromociones, getPromocionesById, modificarPromocionesById }
--- routes/admin/promociones.js
+++ routes/admin/promociones.js
@@ -17,4 +17,63 @@ router.get('/eliminar/:id', async (req, res, next) => {
     res.redirect('/admin/promociones')
 });
 
+router.get('/agregar', (req, res, next) => {
+    res.render('admin/agregar', {
+        layout: 'admin/layout'
+    });
+});
+
+router.post('/agregar', async (req, res, next) => {
+    try {
+        if (req.body.titulo != "" && req.body.cuerpo != "" && req.body.vigencia != "") {
+            await promocionesModel.insertPromociones(req.body);
+            res.redirect('/admin/promociones')
+        } else {
+            res.render('admin/agregar', {
+                layout: 'admin/layout',
+                error: true, 
+                message: 'Todos los campos son requeridos'
+            })
+        }
+    }   catch (error) {
+        console.log(error)
+        res.render('/admin/agregar', {
+            layout: 'admin/layout',
+            error: true, 
+            message: 'No se cargo la promocion'
+        })
+    }
+})
+
+router.get('/modificar/:id', async (req, res, next) => {
+    var id = req.params.id;
+    var promocion = await promocionesModel.getPromocionesById(id);
+    res.render('/admin/modificar', {
+        layout: 'admin/layout',
+        promocion
+    });
+});
+
+router.post('/modificar', async (req, res, next) => {
+    try {
+        var obj = {
+            titulo: req.body.titulo,
+            cuerpo: req.body.cuerpo,
+            vigencia: req.body.vigencia
+        }
+
+    console.log(obj)
+    await promocionesModel.modificarPromocionesById(obj, req.body.id);
+    res.redirect('/admin/promociones');
+    } catch (error) {
+        console.log(error)
+        res.render('/admin/modificar', {
+            layout: 'admin/layout',
+            error: true, 
+            message: 'La promocion no fue modificada'
+        })
+    }
+});
+
+
 module.exports = router;
